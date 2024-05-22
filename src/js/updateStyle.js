fa0376f15fb5be8ad1b55870be8065df402486b2
ZaLLin <zhi.work@mail.ru>
2024-05-22 19:46:59 +0300

    v1.5

--- src/js/updateStyle.js
+++ src/js/updateStyle.js
@@ -1,8 +1,12 @@
 window.onload = function() {
-    var currentValue = localStorage.getItem('feelingsValue') || 3;
-    document.querySelector('input[name="scale"]').value = currentValue;
-    changeBackgroundColor(currentValue);
+    // Проверяем, есть ли значение feelingsValue в localStorage
+    var savedValue = localStorage.getItem('feelingsValue');
+    var initialValue = savedValue ? parseFloat(savedValue) : 3; // Если есть, используем его, иначе устанавливаем 3
+    var currentValue = document.getElementById('feelings');
+    // currentValue.value = initialValue; // Устанавливаем значение элемента input range
+    changeBackgroundColor(initialValue); // Вызываем функцию с начальным значением
 };
+
 
 function changeBackgroundColor(value) {
     var colors = [
@@ -12,7 +16,7 @@ function changeBackgroundColor(value) {
         'linear-gradient(to bottom left, #D7F0F4, #BED3D6)', 
         'linear-gradient(to bottom left, #E8F5D4, #B2CA8E)', 
         'linear-gradient(to bottom left, #FFF0DB, #FFCA86)', 
-        'linear-gradient(to bottom left, #FFDEB1, #FFB85B)'
+        'linear-gradient(to bottom left, #FFDEB1, #FFB85B)'  
     ];
 
     var buttonColors = [
@@ -22,32 +26,29 @@ function changeBackgroundColor(value) {
         '#3B9DB8', 
         '#87CC31', 
         '#EF9300', 
-        '#FD7632'
+        '#FD7632'  
     ];
-    
-    var feelingTexts = [
-        'Очень неприятные', 
-        'Неприятные', 
-        'Почти неприятные', 
-        'Нейтральные', 
-        'Почти приятные', 
-        'Приятные', 
-        'Очень приятные'
-    ];
+        var feelingTexts = [
+            'Очень неприятные', 
+            'Неприятные',      
+            'Почти неприятные', 
+            'Нейтральные',     
+            'Почти приятные',  
+            'Приятные',      
+            'Очень приятные'    
+        ];
 
-    var roundedValue = Math.floor(value);
-    document.body.style.backgroundImage = colors[roundedValue];
-    document.querySelector('h2').textContent = feelingTexts[roundedValue];
-    localStorage.setItem('backgroundColor', colors[roundedValue]);
-    localStorage.setItem('buttonColor', buttonColors[roundedValue]);
-    localStorage.setItem('feelingText', feelingTexts[roundedValue]);
-    localStorage.setItem('feelingsValue', value);
+        var roundedValue = Math.floor(value);
+        document.body.style.backgroundImage = colors[roundedValue];
+        document.querySelector('h2').textContent = feelingTexts[roundedValue];
+        localStorage.setItem('backgroundColor', colors[roundedValue]);
+        localStorage.setItem('buttonColor', buttonColors[roundedValue]);
+        localStorage.setItem('feelingText', feelingTexts[roundedValue]);
+        localStorage.setItem('feelingsValue', value);
 
-    var buttons = document.querySelectorAll('.button:not(.home)');
-    buttons.forEach(function(button) {
-        button.style.backgroundColor = buttonColors[roundedValue];
-        button.style.color = 'white';
-    });
-    var rangeInput = document.querySelector('input[type="range"]');
-    rangeInput.style.setProperty('--thumb-color', buttonColors[roundedValue]);
+        var buttons = document.querySelectorAll('.button:not(.home)');
+        buttons.forEach(function(button) {
+            button.style.backgroundColor = buttonColors[roundedValue];
+            button.style.color = 'white';
+        });
 }
