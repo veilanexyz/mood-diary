8b7ef71611e641954cd45f31f82e7dc3d459039e
veilanexyz <veilanexyz@gmail.com>
2024-06-04 14:38:30 +0300

    working

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-import models, schemas
+import models
+import schemas
 
 def create_mood_entry(db: Session, mood_entry: schemas.MoodEntryCreate):
     db_mood_entry = models.MoodEntry(**mood_entry.dict())
@@ -12,4 +13,4 @@ def get_mood_entries(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.MoodEntry).offset(skip).limit(limit).all()
 
 def get_mood_entries_by_date(db: Session, date: str):
-    return db.query(models.MoodEntry).filter(models.MoodEntry.date.like(f'{date}%')).all()
+    return db.query(models.MoodEntry).filter(models.MoodEntry.date_time.like(f'{date}%')).all()
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
+from datetime import date
 from fastapi import FastAPI, Depends, HTTPException
 from starlette.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-import models, schemas, crud
+import models
+import schemas
+import crud
 from database import SessionLocal, engine
-import datetime
 import os
 
 models.Base.metadata.create_all(bind=engine)
@@ -22,12 +24,6 @@ app.add_middleware(
 current_dir = os.path.dirname(os.path.realpath(__file__))
 static_dir = os.path.join(current_dir, "static", "public")
 
-# Mount the static files directory
-app.mount("/static", StaticFiles(directory=static_dir), name="static")
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Mood Diary app"}
 
 # Dependency
 def get_db():
@@ -37,22 +33,36 @@ def get_db():
     finally:
         db.close()
 
+
 @app.post("/mood_entries/", response_model=schemas.MoodEntry)
-def create_mood_entry(mood_entry: schemas.MoodEntryCreate, db: Session = Depends(get_db)):
+def create_mood_entry(
+    mood_entry: schemas.MoodEntryCreate, db: Session = Depends(get_db)
+):
     return crud.create_mood_entry(db=db, mood_entry=mood_entry)
+
 
 @app.get("/mood_entries/", response_model=list[schemas.MoodEntry])
 def read_mood_entries(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     mood_entries = crud.get_mood_entries(db, skip=skip, limit=limit)
     return mood_entries
 
+
 @app.get("/mood_entries/{date}", response_model=list[schemas.MoodEntry])
-def read_mood_entries_by_date(date: str, db: Session = Depends(get_db)):
+def read_mood_entries_by_date(date: date, db: Session = Depends(get_db)):
     mood_entries = crud.get_mood_entries_by_date(db, date=date)
     if not mood_entries:
-        raise HTTPException(status_code=404, detail="No mood entries found for this date")
+        raise HTTPException(
+            status_code=404, detail="No mood entries found for this date"
+        )
     return mood_entries
+
+
+# Mount the static files directory
+app.mount("/src", StaticFiles(directory="static/src"), name="js & css")
+app.mount("/", StaticFiles(directory="static/public", html=True), name="html")
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
