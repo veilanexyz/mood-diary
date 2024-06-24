9c9c340bc2e13370957d96bf33db66ff57147a17
Tog91l <114440739+Tog91l@users.noreply.github.com>
2024-06-24 16:32:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createAssistant, createSmartappDebugger } from '@salutejs/client';
+import './Styles/App.scss';
+import { BrowserRouter as Router, Route, Routes /*useNavigate*/ } from 'react-router-dom';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
 import Page3 from './pages/page3';
 import DonePage from './pages/done_page';
 import Homepage from './pages/homepage';
-import './Styles/App.scss';
 
-const App = () => {
-  return (
-    <Router>
+const initializeAssistant = (getState /*: any*/, getRecoveryState) => {
+  if (process.env.NODE_ENV === 'development') {
+    return createSmartappDebugger({
+      token: process.env.REACT_APP_TOKEN ?? '',
+      initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+      getState,
+      // getRecoveryState: getState,
+      nativePanel: {
+        defaultText: 'ччччччч',
+        screenshotMode: false,
+        tabIndex: -1,
+      },
+    });
+  } else {
+    return createAssistant({ getState });
+  }
+};
+
+class AppWithAssist extends React.Component {
+ // navigate = useNavigate();
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+
+    this.state = {
+      //notes: [{ id: Math.random().toString(36).substring(7), title: 'тест' }],
+      period: null,
+      description: null,
+      factors: null
+    };
+
+    this.assistant = initializeAssistant(() => this.getStateForAssistant());
+
+    this.assistant.on('data', (event /*: any*/) => {
+      console.log(`assistant.on(data)`, event);
+      if (event.type === 'character') {
+        console.log(`assistant.on(data): character: "${event?.character?.id}"`);
+      } else if (event.type === 'insets') {
+        console.log(`assistant.on(data): insets`);
+      } else {
+        const { action } = event;
+        this.dispatchAssistantAction(action);
+      }
+    });
+
+    this.assistant.on('start', (event) => {
+      let initialData = this.assistant.getInitialData();
+
+      console.log(`assistant.on(start)`, event, initialData);
+    });
+
+    this.assistant.on('command', (event) => {
+      console.log(`assistant.on(command)`, event);
+    });
+
+    this.assistant.on('error', (event) => {
+      console.log(`assistant.on(error)`, event);
+    });
+
+    this.assistant.on('tts', (event) => {
+      console.log(`assistant.on(tts)`, event);
+    });
+  }
+
+  getStateForAssistant() {
+    console.log('getStateForAssistant: this.state:', this.state);
+    const state = {
+      item_selector: {
+        period: this.state.period,
+        feelingDescription: this.state.description,
+        influenceFactors: this.state.factors,
+
+        /*ignored_words: [
+          'добавить',
+          'установить',
+          'запиши',
+          'поставь',
+          'закинь',
+          'напомнить', // addNote.sc
+          'удалить',
+          'удали', // deleteNote.sc
+          'выполни',
+          'выполнил',
+          'сделал', // выполнил|сделал
+        ], */
+      },
+    };
+    console.log('getStateForAssistant: state:', state);
+    return state;
+  }
+
+  dispatchAssistantAction(action) {
+    console.log('dispatchAssistantAction', action);
+    if (action) {
+      switch (action.type) {
+        case 'save_mood_period':
+          return this.save_mood_period(action);
+
+        case 'save_feeling_description':
+          return this.save_feeling_description(action);
+
+        case 'save_influence_factors':
+          return this.save_influence_factors(action);
+
+        default:
+          throw new Error();
+      }
+    }
+  }
+  save_mood_period(action){
+    console.log('save_mood_period', action);
+    this.setState({period: action.period }, () => {
+      window.location.href = '/page2';
+    });
+  }
+
+  save_feeling_description(action){
+    console.log('save_feeling_description', action);
+    this.setState({ feelingDescription: action.description});
+     // window.location.href = '/page3';
+   // });
+  }
+
+  save_influence_factors(action){
+    console.log('save_influence_factors', action);
+    this.setState({ influenceFactors: action.factors});
+      //window.location.href = '/done_page';
+    //});
+  }
+  render() {
+    console.log('render');
+    return (
+      <Router>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
+       
           <Route path="/page1" element={<Page1 />} />
           <Route path="/page2" element={<Page2 />} />
           <Route path="/page3" element={<Page3 />} />
           <Route path="/done_page" element={<DonePage />} />
         </Routes>
-    </Router>
-  );
-};
+      </Router>
+    );
+  }
+}
 
-export default App;
+export default AppWithAssist;
